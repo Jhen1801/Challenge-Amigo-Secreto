7da4e2da3574e218525550b5e1be88b1ce2eb00f
Jhenifer Correa <jhenifercorreapsi@gmail.com>
2025-02-06 00:22:32 -0500

    upload file

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 /* AMIGO SECRETO - Jhenifer Correa Vasquez*/
 
-//Crear un array para almacenar los nombres
+//1.Crear un array para almacenar los nombres
 let amigos = [];
-console.log(amigos);
 
-//Implementa una función para agregar amigos
+//2.Implementa una función para agregar amigos
 function agregarAmigo() {
     //Capturar el valor del campo de entrada
-    let nuevoAmigo = document.getElementById('amigo');
-    console.log(nuevoAmigo);
-    //Validar la entrada
-    if (nuevoAmigo == "") {
-        alert('ERROR: Por favor, inserte un nombre válido');
-        //Limpiar el campo de entrada
-        document.getElementById('input-name').setAttribute('Escribe un nombre','true');
-    } else {
-        //Si el nombre es válido, actualizar el array de amigos
-        amigos.push(nuevoAmigo);
+    let listaAmigos = document.getElementById("amigo");
+    //Validar la entrada y verificar que no quede en blanco
+    if (listaAmigos.value.trim() !== "") {
+        //Si el nombre es válido o no está en blanco, actualizar el array de amigos
+        amigos.push(listaAmigos.value.trim());
         console.log(amigos);
         //Limpiar el campo de entrada
-        document.getElementById('input-name').setAttribute('Escribe un nombre','true');
+        listaAmigos.value = "";
+    } else {
+        //si el nombre es invalido
+        alert('ERROR: Por favor, inserte un nombre válido');
     }
 }
 
