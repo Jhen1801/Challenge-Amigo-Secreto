a92cf4df18d825ebe931f3e1ea9e6e5c3d0bb300
Jhenifer Correa <jhenifercorreapsi@gmail.com>
2025-02-05 23:25:56 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/* AMIGO SECRETO - Jhenifer Correa Vasquez - 1148697471 */
+
+//Crear un array para almacenar los nombres
+let amigos = [];
+
+//Implementa una función para agregar amigos
+function agregarAmigo() {
+    //Capturar el valor del campo de entrada
+    let nuevoAmigo = document.getElementById('amigo');
+    console.log(nuevoAmigo);
+    //Validar la entrada
+    if (nuevoAmigo == "") {
+        alert('ERROR: Por favor, inserte un nombre válido');
+        //Limpiar el campo de entrada
+        document.getElementById('input-name').setAttribute('Escribe un nombre','true');
+    } else {
+        //Si el nombre es válido, actualizar el array de amigos
+        amigos = nuevoAmigo.push();
+        console.log(amigos);
+        //Limpiar el campo de entrada
+        document.getElementById('input-name').setAttribute('Escribe un nombre','true');
+    }
+}
+
