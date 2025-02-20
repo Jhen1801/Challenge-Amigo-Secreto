6fa485d0aec09155aab9caa3afec2b68259958b7
Jhenifer Correa <jhenifercorreapsi@gmail.com>
2025-02-19 19:59:15 -0500

    update app.js

--- app.js
+++ app.js
@@ -7,16 +7,53 @@ let amigos = [];
 function agregarAmigo() {
     //Capturar el valor del campo de entrada
     let listaAmigos = document.getElementById("amigo");
+    let nombre = listaAmigos.value.trim();
     //Validar la entrada y verificar que no quede en blanco
-    if (listaAmigos.value.trim() !== "") {
-        //Si el nombre es válido o no está en blanco, actualizar el array de amigos
-        amigos.push(listaAmigos.value.trim());
-        console.log(amigos);
-        //Limpiar el campo de entrada
-        listaAmigos.value = "";
-    } else {
-        //si el nombre es invalido
+    if (nombre === "") {
         alert('ERROR: Por favor, inserte un nombre válido');
+        return;
     }
+    //verificar que el nombre no se repita
+    if (amigos.includes(nombre)) {
+        alert("ERROR: Ese nombre ya fue agregado");
+        listaAmigos.value = "";
+        return;
+    }
+    //Si el nombre es válido o no está en blanco, actualizar el array de amigos
+    amigos.push(nombre);
+    console.log(amigos);
+    // Llamar la función para actualizar la lista en el HTML
+    actualizarAmigos();
+    //Limpiar el campo de entrada
+    listaAmigos.value = "";
 }
 
+//3. Implementa una función para actualizar la lista de amigos
+function actualizarAmigos() {
+    //Obtener el elemento de la lista
+    let listaAmigos = document.getElementById("listaAmigos");
+    //Limpiar la lista existente
+    listaAmigos.innerHTML = ""
+    //Iterar sobre el arreglo y Agregar elementos a la lista
+    for (let amigo of amigos) {
+        let nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = amigo;
+        listaAmigos.appendChild(nuevoElemento);
+    }
+    console.log(listaAmigos);
+}
+
+//4. Implementa una función para sortear los amigos
+function sortearAmigo() {
+    //Validar que haya amigos disponibles
+    if (amigos.length === 0) {
+        resultado.innerHTML = "<p style='color: red;'>ERROR: No hay amigos en la lista</p>";
+        return;
+    }
+    //Generar un índice aleatorio
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    // Obtener el nombre sorteado
+    let amigoSorteado = amigos[indiceAleatorio];
+    // Mostrar el resultado en la página
+    resultado.innerHTML = `<p>🎉 ¡El amigo secreto es <strong>${amigoSorteado}</strong>! 🎁</p>`;
+}
